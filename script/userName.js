0c8c4b25c5f283eb8b68dcb94bc48122e22e45fe
Alexxxsandoor <v.verstukov14@gmail.com>
2022-03-06 03:52:33 +0200

    added css style for scroll line + added output user info

--- script/userName.js
+++ script/userName.js
@@ -4,12 +4,12 @@ const user = [
 		firstName: "Александр",
 		lastName: "Верстуков",
 		dateOfBirth: "08/05/2000",
-		phone: "+38(099)238-55-34",
+		phone: "+380992385534",
 		login: "Alexxxsandoor",
 		password: "qwerty12345",
 		dateRegistration: "06/03/2022",
 		email: "v.verstukov14@gmail.com",
-		startCourse: "7",//!course: 7,8,9,10,11,DPA,ZNO
+		startCourse: 7,//!course: 7,8,9,10,11,DPA,ZNO
 		completedСourses: [],
 		completedThemes: [
 			{ id: 70 },
@@ -21,7 +21,7 @@ const user = [
 		firstName: "Иванка",
 		lastName: "Павлик",
 		dateOfBirth: "18/10/2000",
-		phone: "+38(097)529-73-01",
+		phone: "+380975297301",
 		login: "ivibambi",
 		password: "oiuqwehjhk@123123",
 		dateRegistration: "01/02/2021",
@@ -44,6 +44,21 @@ const userOutput = (user, id) => {
 
 	const idFirstName = document.querySelector("#firstname");
 	idFirstName.innerHTML = user[id].firstName;
+
+	const userLogin = document.querySelector(".login");
+	userLogin.append(user[id].login)
+
+	const userPhone = document.querySelector(".phone");
+	userPhone.append(user[id].phone)
+
+	const userEmail = document.querySelector(".email");
+	userEmail.append(user[id].email)
+
+	const userCourse = document.querySelector(".course");
+
+	if (typeof user[id].startCourse == "number") userCourse.append(user[id].startCourse + " класс")
+	else userCourse.append(user[id].startCourse)
+
 }
 
 
